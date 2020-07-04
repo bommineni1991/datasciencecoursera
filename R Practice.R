852fcbc651157ee210cbe15fbdcf60343792e5f8
bommineni1991 <bommineni1991@gmail.com>
2020-07-05 01:31:14 +0800

    This is markdown file

---
+++ R Practice.R
@@ -0,0 +1,19 @@
+install.packages("ggplot2") # to install the packages
+
+library(ggplot2) # to load the installed packages
+
+old.packages() # to view the packages
+
+update.packages() # to update the packages
+
+detach("package:ggplot2",unload=TRUE) # to unload the packages
+
+remove.packages("ggplot2") # to remove the packages
+
+version # to know the R version
+
+sessionInfo() # to know the session information
+
+help(package = "ggplot2") # to see the selected package uses
+
+install.packages("KernSmooth") # to install the packages
